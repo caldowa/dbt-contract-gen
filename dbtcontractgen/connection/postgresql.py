418607c182ed912f10d76abec49463ac26670606
caldowa <arnaud.caldow@gmail.com>
2024-08-18 20:32:22 +0200

    Created postgreSQLConnection

---
+++ dbtcontractgen/connection/postgresql.py
@@ -0,0 +1,20 @@
+import psycopg2
+
+from dbtcontractgen.connection import DatabaseConnection
+
+
+class PostgreSQLConnection(DatabaseConnection):
+    def connect(self):
+        self.conn = psycopg2.connect(
+            host=self.host,
+            user=self.user,
+            password=self.password,
+            database=self.database,
+            port=self.port,
+        )
+        self.cursor = self.conn.cursor()
+
+    def run_query(self, query: str):
+        with self.cursor as cursor:
+            cursor.execute(query)
+            return cursor.fetchall()
