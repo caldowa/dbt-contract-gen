8684deafe81f338c11a0b41d1783f8b293ae53fa
caldowa <arnaud.caldow@gmail.com>
2024-08-18 20:18:36 +0200

    Added postgresql connection

--- dbtcontractgen/utils/database.py
+++ dbtcontractgen/utils/database.py
@@ -1,5 +1,7 @@
 from abc import ABC, abstractmethod
 
+import psycopg2
+
 
 class DatabaseConnection(ABC):
     def __init__(self, host: str, user: str, password: str, database: str, port: int):
@@ -38,3 +40,20 @@ class DatabaseConnection(ABC):
         if self.cursor:
             self.cursor.close()
         self.close()
+
+
+class PostgreSQLConnection(DatabaseConnection):
+    def connect(self):
+        self.conn = psycopg2.connect(
+            host=self.host,
+            user=self.user,
+            password=self.password,
+            database=self.database,
+            port=self.port,
+        )
+        self.cursor = self.conn.cursor()
+
+    def run_query(self, query: str):
+        with self.cursor as cursor:
+            cursor.execute(query)
+            return cursor.fetchall()
