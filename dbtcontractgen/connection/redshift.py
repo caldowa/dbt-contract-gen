6f6d56c5e2ff7397a9b18b820b87e1cd68332891
caldowa <arnaud.caldow@gmail.com>
2024-08-18 20:32:37 +0200

    Created redshift connection

---
+++ dbtcontractgen/connection/redshift.py
@@ -0,0 +1,16 @@
+import redshift_connector
+
+from dbtcontractgen.connection import DatabaseConnection
+
+
+class RedsiftConnection(DatabaseConnection):
+    def connect(self):
+        self.conn = redshift_connector.connect(
+            host=self.host, database=self.database, user=self.user, password=self.password, port=self.port
+        )
+        self.cursor = self.conn.cursor()
+
+    def run_query(self, query: str):
+        with self.cursor as cursor:
+            cursor.execute(query)
+            return cursor.fetchall()
